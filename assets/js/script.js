11666a6f3f95261d66fc398a7f4d4f633277c596
Fausnightm <m.fausnight@lafilm.edu>
2021-06-26 18:27:23 -0400

    declare variable in js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Display today's date
+var todaydate = moment().format('dddd, MMM Do YYYY')
+$('#currentDay').html(todaydate);
+
+$(document).ready(function() {
+    // saveBtn click listener
+    // get nearby values using jQuery
+    
+    // save text in local storage
+})
+
