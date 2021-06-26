67e29439159b7a9cd2c2058ad525653059941556
Fausnightm <m.fausnight@lafilm.edu>
2021-06-26 19:09:53 -0400

    declare variables for current time and parse out variable for each time block

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,29 @@ $('#currentDay').html(todaydate);
 
 $(document).ready(function() {
     // saveBtn click listener
-    // get nearby values using jQuery
-    
+    $('.saveBtn').on('click', function (){
+        // get nearby values using jQuery
+        var text = $(this).siblings('.description').val()
+        var time = $(this).parent().attr('id');
+    })
     // save text in local storage
+    localStorage.setItem(time,text);
 })
+
+function timeTracker() {
+    // get current number of hours
+    var timeNow = moment().hour();
+
+    // loop over time blocks
+    $('.time-block').each(function (){
+        var blockTime = parseInt($(this).attr('id').split('hour')[1]);
+        // start for loops
+    })
+}
+
+// loop over time blocks
+
+// start function declare parse variable
+    // check the time and addd the classes for background indicators
+
 
