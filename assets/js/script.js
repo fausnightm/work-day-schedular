8cf641745eef93c5fe8bfe154d065a548f190a75
Fausnightm <m.fausnight@lafilm.edu>
2021-06-26 20:34:00 -0400

    added for loop to generate classes based on time signature

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,12 +21,30 @@ function timeTracker() {
     $('.time-block').each(function (){
         var blockTime = parseInt($(this).attr('id').split('hour')[1]);
         // start for loops
+            // check the time and addd the classes for background indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+            $(this).addClass('past')
+        }
+
+        else if (blockTime === timeNow) {
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+            $(this).addClass('present');
+        }
+
+        else {
+            $(this).removeClass('present');
+            $(this).removeClass('past');
+            $(this).addClass('future');
+        }
+
     })
 }
 
 // loop over time blocks
 
 // start function declare parse variable
-    // check the time and addd the classes for background indicators
 
 
